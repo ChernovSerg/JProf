c4b58e7f1ddcb8dfea54c6fd3c166c4fb82b3e73
Sergey Chernov <chernov_serg@mail.ru>
2019-02-04 23:38:57 +0300

    Les04. Работа с файлами

--- src/Les04/TestReadFileScanner.java
+++ src/Les04/TestReadFileScanner.java
@@ -1,18 +1,22 @@
 /*
  * 02.02.19 23:13 Created by Sergey Chernov.
+ *
+ * Создайте в package текстовый файл и клас, внесите в файл некий текст.
+ * Выведите на экран содержимое файла.
  */
 
 package Les04;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.nio.file.Paths;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class TestReadFileScanner {
     public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner sc = new Scanner(new FileReader("test.txt"));
+        Scanner sc = new Scanner(new FileReader(".\\src\\Les04\\test.txt"));
         sc.useLocale(Locale.US);
 
         while (sc.hasNextLine()) {
